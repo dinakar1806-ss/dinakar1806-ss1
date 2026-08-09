2dcf592ab52fabb9e5a9695a22a53ab09fcad6fb
Dinakar SS <dinakarss18@gmail.com>
2026-08-09 11:03:31 +0530

    Update MaskedPhoneNumber.java

--- MaskedPhoneNumber.java
+++ MaskedPhoneNumber.java
@@ -5,12 +5,12 @@ public class MaskedPhoneNumber {
 
     static String maskPhoneNumber(String phone) {
 
-        // Check length
+        
         if (phone.length() != 10) {
             return "Invalid phone number";
         }
 
-        // Check whether all characters are digits
+        
         for (int i = 0; i < phone.length(); i++) {
 
             if (!Character.isDigit(phone.charAt(i))) {
