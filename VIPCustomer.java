0392970194d96eea3725a440f1d4c1cd7329a2e6
Dinakar SS <dinakarss18@gmail.com>
2026-08-09 19:23:52 +0530

    Add files via upload

---
+++ VIPCustomer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class VIPCustomer {
+
+    
+    public static String validateCustomerId(String customerId) {
+
+        if (customerId.startsWith("VIP-")) {
+            return "VIP Customer";
+        } else {
+            return "Regular Customer";
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Customer ID: ");
+        String customerId = sc.nextLine();
+
+        String result = validateCustomerId(customerId);
+
+        System.out.println("Result: " + result);
+
+        sc.close();
+    }
+}
